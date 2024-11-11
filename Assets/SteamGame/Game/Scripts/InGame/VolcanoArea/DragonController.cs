ca561df62b650ee91d6b6edb2edde9e13dd1b1aa
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2024-11-12 03:44:01 +0900

    dragonFly

--- Assets/SteamGame/Game/Scripts/InGame/VolcanoArea/DragonController.cs
+++ Assets/SteamGame/Game/Scripts/InGame/VolcanoArea/DragonController.cs
@@ -7,9 +7,16 @@ namespace TechC
     public class DragonController : MonoBehaviour
     {
         [SerializeField] private GameObject wayPointsParent;
+        [SerializeField] private GameObject dragonObj;
         [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float rotationSpeed = 2f; // 回転速度を追加
+        [SerializeField] private string downAnim, upAnim,forwardAnim;
+
+        [Header("Reference")]
+        [SerializeField] private Animator anim;
         private Transform[] wayPoints;
         private int currentWaypointIndex = 0;
+        private bool isMoving = true;
 
         private void Awake()
         {
@@ -31,16 +38,40 @@ namespace TechC
 
         private IEnumerator Fly()
         {
-            while (true)
+            while (isMoving)
             {
                 Transform targetWaypoint = wayPoints[currentWaypointIndex];
 
-                while (Vector3.Distance(transform.position, targetWaypoint.position) > 0.1f)
+                // 次のウェイポイントの方向に向かせる
+                Quaternion targetRotation = Quaternion.LookRotation(targetWaypoint.position - dragonObj.transform.position);
+                while (Vector3.Distance(dragonObj.transform.position, targetWaypoint.position) > 0.1f)
                 {
-                    transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, moveSpeed * Time.deltaTime);
+                    // 回転をスムーズに行う
+                    dragonObj.transform.rotation = Quaternion.Slerp(dragonObj.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+                    // 移動する
+                    dragonObj.transform.position = Vector3.MoveTowards(dragonObj.transform.position, targetWaypoint.position, moveSpeed * Time.deltaTime);
+
                     yield return null;
                 }
 
+                // 上下判定
+                Vector3 directionToNextWaypoint = targetWaypoint.position - dragonObj.transform.position;
+                bool isUpward = directionToNextWaypoint.y > 0;
+
+                if (!isUpward)
+                {
+                    anim.SetBool(upAnim, true);
+                    anim.SetBool(downAnim, false);
+                    Debug.Log("Up");
+                }
+                else
+                {
+                    anim.SetBool(upAnim, false);
+                    anim.SetBool(downAnim, true);
+                }
+
+                // 次のウェイポイントに進む
                 currentWaypointIndex = (currentWaypointIndex + 1) % wayPoints.Length;
 
                 yield return null;
