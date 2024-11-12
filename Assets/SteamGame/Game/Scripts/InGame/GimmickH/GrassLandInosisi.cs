ebfd3176521aca8f7e4304e6ab000da0c78d2a3c
Millan50 <1015aika256431@gmail.com>
2024-11-12 19:31:34 +0900

    草原のギミック＋塔作成＋ステージ作成

--- Assets/SteamGame/Game/Scripts/InGame/GimmickH/GrassLandInosisi.cs
+++ Assets/SteamGame/Game/Scripts/InGame/GimmickH/GrassLandInosisi.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace TechC
 {
     public class GrassLandInosisi : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private Transform _LeftEdge;
+        [SerializeField] private Transform _RightEdge;
+
+        [SerializeField] private Transform inosisi;
+
+        private float MoveSpeed = 3.0f;
+        private int direction = 1;
         
-        }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
-        
+            if (transform.position.x >= _RightEdge.position.x)
+                direction = -1;
+            if (transform.position.x <= _LeftEdge.position.x)
+                direction = 1;
+            inosisi.transform.position = new Vector3(transform.position.x + MoveSpeed * Time.fixedDeltaTime * direction, 0, 0);
         }
     }
 }
