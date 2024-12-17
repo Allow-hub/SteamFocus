07ef7b35c6d0822770dba525c9897db0d97cd512
Masakatsu Wawanabe <masakatsu@unslow.com>
2024-12-17 15:36:30 +0900

    アバター名表示の機能を正常化

--- Assets/SteamGame/Game/Scripts/NetWork/AvatarNameDisplay.cs
+++ Assets/SteamGame/Game/Scripts/NetWork/AvatarNameDisplay.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 namespace TechC
 {
+    [RequireComponent(typeof (TMP_Text))]
     public class AvatarNameDisplay : MonoBehaviourPunCallbacks
     {
         private void Start()
         {
-            var nameLabel = GetComponent<TextMeshPro>();
+            var nameLabel = GetComponent<TMP_Text>();
+            
             // プレイヤー名とプレイヤーIDを表示する
             nameLabel.text = $"{photonView.Owner.NickName}({photonView.OwnerActorNr})";
         }
